e79a8000de8266fb570f0a8312aa0ade2db1abc4
priyanka8072 <ps79861n@pace.edu>
2016-12-05 12:16:49 -0500

    UI corrections

--- src/com/musicStore/controller/MyController.java
+++ src/com/musicStore/controller/MyController.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.musicStore.service.MyService;
@@ -21,8 +21,8 @@ public class MyController {
 		this.myservice = myservice;
 	}
 
-	@RequestMapping(value = "/searchSong", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody Map<Object, Object> searchSong(@RequestParam String name) {
+	@RequestMapping(value = "{name}", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody Map<Object, Object> searchSong(@PathVariable String name) {
 
 		Map<Object, Object> result = new HashMap<Object, Object>();
 		// result = myservice.getSong(name);
